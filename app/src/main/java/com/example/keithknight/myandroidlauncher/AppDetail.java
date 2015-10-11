26d7b5c47c64916d37b60989cddfe333690b8db9
krknight <keith.knight@rockwellcollins.com>
2015-10-11 15:44:45 -0700

    Fetch all the Apps that have a category of Intent.CATEGORY_LAUNCHER

---
+++ app/src/main/java/com/example/keithknight/myandroidlauncher/AppDetail.java
@@ -0,0 +1,12 @@
+package com.example.keithknight.myandroidlauncher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by keithknight on 10/11/15.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
